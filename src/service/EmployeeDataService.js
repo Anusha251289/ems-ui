ad034f1a59e47aa37ff43f49ac0dd71d9f862ef3
Anusha251289 <anushakrishnan4@gmail.com>
2021-05-06 21:10:19 -0400

    Added API url; updated custom env var

--- src/service/EmployeeDataService.js
+++ src/service/EmployeeDataService.js
@@ -3,16 +3,27 @@ import axios from 'axios'
 class EmployeeDataService {
 
     getEmployees() {
-        console.log("env", process.env); 
-        return axios.get('http://localhost:8080/api/employees');
+        if (process.env.REACT_APP_ENV === "LOCAL")
+            return axios.get('http://localhost:8080/api/employees');
+        else if (process.env.REACT_APP_ENV === "PROD")
+            return axios.get('https://emp-management-api.herokuapp.com/api/employees');
     }
 
     deleteEmployee(id) {
-        return axios.delete(`http://localhost:8080/api/employee/${id}`);
+
+        if (process.env.REACT_APP_ENV === "LOCAL")
+            return axios.delete(`http://localhost:8080/api/employee/${id}`);
+        else if (process.env.REACT_APP_ENV === "PROD")
+            return axios.delete(`https://emp-management-api.herokuapp.com/api/employee/${id}`);
+        //return axios.delete(`http://localhost:8080/api/employee/${id}`);
     }
 
     saveEmployee(employee) {
-        return axios.post(`http://localhost:8080/api/employee`,employee);
+        if (process.env.REACT_APP_ENV === "LOCAL")
+            return axios.post(`http://localhost:8080/api/employee`, employee);
+        else if (process.env.REACT_APP_ENV === "PROD")
+            return axios.post(`https://emp-management-api.herokuapp.com/api/employee`, employee);
+        //return axios.post(`http://localhost:8080/api/employee`,employee);
     }
 }
 
