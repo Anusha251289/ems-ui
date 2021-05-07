550cf1a23808e8d51ccb615aa91bbc5a8ab5a14d
Anusha251289 <anushakrishnan4@gmail.com>
2021-05-06 20:53:24 -0400

    Testing env var

--- src/service/EmployeeDataService.js
+++ src/service/EmployeeDataService.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 class EmployeeDataService {
 
     getEmployees() {
+        console.log("env", process.env); 
         return axios.get('http://localhost:8080/api/employees');
     }
 
